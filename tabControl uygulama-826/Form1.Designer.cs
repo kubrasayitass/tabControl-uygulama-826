8a0df3360aa12e0314482e432e622dfdc9634cde
kubrasayitass <145451189+kubrasayitass@users.noreply.github.com>
2024-02-13 11:01:45 +0300

    Add files via upload

---
+++ tabControl uygulama-826/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace tabControl_uygulama_826
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.lblAdSoyad = new System.Windows.Forms.Label();
+            this.lblTel = new System.Windows.Forms.Label();
+            this.lblAdres = new System.Windows.Forms.Label();
+            this.txtAdSoyad = new System.Windows.Forms.TextBox();
+            this.txtTel = new System.Windows.Forms.TextBox();
+            this.txtAdres = new System.Windows.Forms.TextBox();
+            this.lblCorba = new System.Windows.Forms.Label();
+            this.lblSalata = new System.Windows.Forms.Label();
+            this.lblYemek = new System.Windows.Forms.Label();
+            this.lblTatli = new System.Windows.Forms.Label();
+            this.nCorba = new System.Windows.Forms.NumericUpDown();
+            this.nSalata = new System.Windows.Forms.NumericUpDown();
+            this.nYemek = new System.Windows.Forms.NumericUpDown();
+            this.nTatli = new System.Windows.Forms.NumericUpDown();
+            this.txtBilgi = new System.Windows.Forms.TextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nCorba)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nSalata)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nYemek)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nTatli)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(66, 69);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(337, 193);
+            this.tabControl1.TabIndex = 0;
+            this.tabControl1.SelectedIndexChanged += new System.EventHandler(this.tabControl1_SelectedIndexChanged);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.txtAdres);
+            this.tabPage1.Controls.Add(this.txtTel);
+            this.tabPage1.Controls.Add(this.txtAdSoyad);
+            this.tabPage1.Controls.Add(this.lblAdres);
+            this.tabPage1.Controls.Add(this.lblTel);
+            this.tabPage1.Controls.Add(this.lblAdSoyad);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(329, 167);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "müşteri";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.nTatli);
+            this.tabPage2.Controls.Add(this.nYemek);
+            this.tabPage2.Controls.Add(this.nSalata);
+            this.tabPage2.Controls.Add(this.nCorba);
+            this.tabPage2.Controls.Add(this.lblTatli);
+            this.tabPage2.Controls.Add(this.lblYemek);
+            this.tabPage2.Controls.Add(this.lblSalata);
+            this.tabPage2.Controls.Add(this.lblCorba);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(329, 167);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "sipariş";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.txtBilgi);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(329, 167);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "hesap";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // lblAdSoyad
+            // 
+            this.lblAdSoyad.AutoSize = true;
+            this.lblAdSoyad.Location = new System.Drawing.Point(21, 25);
+            this.lblAdSoyad.Name = "lblAdSoyad";
+            this.lblAdSoyad.Size = new System.Drawing.Size(53, 13);
+            this.lblAdSoyad.TabIndex = 0;
+            this.lblAdSoyad.Text = "ad soyad:";
+            // 
+            // lblTel
+            // 
+            this.lblTel.AutoSize = true;
+            this.lblTel.Location = new System.Drawing.Point(21, 67);
+            this.lblTel.Name = "lblTel";
+            this.lblTel.Size = new System.Drawing.Size(42, 13);
+            this.lblTel.TabIndex = 1;
+            this.lblTel.Text = "telefon:";
+            // 
+            // lblAdres
+            // 
+            this.lblAdres.AutoSize = true;
+            this.lblAdres.Location = new System.Drawing.Point(21, 106);
+            this.lblAdres.Name = "lblAdres";
+            this.lblAdres.Size = new System.Drawing.Size(36, 13);
+            this.lblAdres.TabIndex = 2;
+            this.lblAdres.Text = "adres:";
+            // 
+            // txtAdSoyad
+            // 
+            this.txtAdSoyad.Location = new System.Drawing.Point(98, 25);
+            this.txtAdSoyad.Name = "txtAdSoyad";
+            this.txtAdSoyad.Size = new System.Drawing.Size(100, 20);
+            this.txtAdSoyad.TabIndex = 3;
+            // 
+            // txtTel
+            // 
+            this.txtTel.Location = new System.Drawing.Point(98, 60);
+            this.txtTel.Name = "txtTel";
+            this.txtTel.Size = new System.Drawing.Size(100, 20);
+            this.txtTel.TabIndex = 4;
+            // 
+            // txtAdres
+            // 
+            this.txtAdres.Location = new System.Drawing.Point(98, 99);
+            this.txtAdres.Name = "txtAdres";
+            this.txtAdres.Size = new System.Drawing.Size(100, 20);
+            this.txtAdres.TabIndex = 5;
+            // 
+            // lblCorba
+            // 
+            this.lblCorba.AutoSize = true;
+            this.lblCorba.Location = new System.Drawing.Point(19, 31);
+            this.lblCorba.Name = "lblCorba";
+            this.lblCorba.Size = new System.Drawing.Size(37, 13);
+            this.lblCorba.TabIndex = 0;
+            this.lblCorba.Text = "çorba:";
+            // 
+            // lblSalata
+            // 
+            this.lblSalata.AutoSize = true;
+            this.lblSalata.Location = new System.Drawing.Point(19, 57);
+            this.lblSalata.Name = "lblSalata";
+            this.lblSalata.Size = new System.Drawing.Size(35, 13);
+            this.lblSalata.TabIndex = 1;
+            this.lblSalata.Text = "salata";
+            // 
+            // lblYemek
+            // 
+            this.lblYemek.AutoSize = true;
+            this.lblYemek.Location = new System.Drawing.Point(19, 91);
+            this.lblYemek.Name = "lblYemek";
+            this.lblYemek.Size = new System.Drawing.Size(62, 13);
+            this.lblYemek.TabIndex = 2;
+            this.lblYemek.Text = "ana yemek:";
+            // 
+            // lblTatli
+            // 
+            this.lblTatli.AutoSize = true;
+            this.lblTatli.Location = new System.Drawing.Point(19, 125);
+            this.lblTatli.Name = "lblTatli";
+            this.lblTatli.Size = new System.Drawing.Size(26, 13);
+            this.lblTatli.TabIndex = 3;
+            this.lblTatli.Text = "tatlı:";
+            // 
+            // nCorba
+            // 
+            this.nCorba.Location = new System.Drawing.Point(112, 31);
+            this.nCorba.Name = "nCorba";
+            this.nCorba.Size = new System.Drawing.Size(120, 20);
+            this.nCorba.TabIndex = 4;
+            // 
+            // nSalata
+            // 
+            this.nSalata.Location = new System.Drawing.Point(112, 57);
+            this.nSalata.Name = "nSalata";
+            this.nSalata.Size = new System.Drawing.Size(120, 20);
+            this.nSalata.TabIndex = 5;
+            // 
+            // nYemek
+            // 
+            this.nYemek.Location = new System.Drawing.Point(112, 83);
+            this.nYemek.Name = "nYemek";
+            this.nYemek.Size = new System.Drawing.Size(120, 20);
+            this.nYemek.TabIndex = 6;
+            // 
+            // nTatli
+            // 
+            this.nTatli.Location = new System.Drawing.Point(112, 118);
+            this.nTatli.Name = "nTatli";
+            this.nTatli.Size = new System.Drawing.Size(120, 20);
+            this.nTatli.TabIndex = 7;
+            // 
+            // txtBilgi
+            // 
+            this.txtBilgi.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtBilgi.Location = new System.Drawing.Point(3, 3);
+            this.txtBilgi.Name = "txtBilgi";
+            this.txtBilgi.Size = new System.Drawing.Size(323, 20);
+            this.txtBilgi.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nCorba)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nSalata)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nYemek)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nTatli)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TextBox txtAdres;
+        private System.Windows.Forms.TextBox txtTel;
+        private System.Windows.Forms.TextBox txtAdSoyad;
+        private System.Windows.Forms.Label lblAdres;
+        private System.Windows.Forms.Label lblTel;
+        private System.Windows.Forms.Label lblAdSoyad;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.NumericUpDown nTatli;
+        private System.Windows.Forms.NumericUpDown nYemek;
+        private System.Windows.Forms.NumericUpDown nSalata;
+        private System.Windows.Forms.NumericUpDown nCorba;
+        private System.Windows.Forms.Label lblTatli;
+        private System.Windows.Forms.Label lblYemek;
+        private System.Windows.Forms.Label lblSalata;
+        private System.Windows.Forms.Label lblCorba;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TextBox txtBilgi;
+    }
+}
+
---
+++ tabControl uygulama-826/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tabControl_uygulama_826
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (tabControl1.SelectedIndex == 2)
+            {
+                decimal hesap = 0;
+                String mesaj = $"ad soyad:{txtAdSoyad.Text}\r\n" +
+                    $"telefon:{txtTel.Text}\r\n" +
+                $"adres:{txtAdres.Text}\r\n\r\n";
+                if (nCorba.Value > 0)
+                {
+                    hesap += nCorba.Value * 60;
+                    mesaj += $"Çorba =      {nCorba.Value * 60}\r\n";
+
+                    if(nSalata.Value > 0)
+                    {
+                        hesap += nSalata.Value * 30;
+                        mesaj += $"yemek=       {nSalata.Value * 30}\r\n"; ;
+                        if(nYemek.Value > 0)
+                        {
+                            hesap  += nYemek.Value * 100;
+                            mesaj += $"yemek =      {nYemek.Value*100}\r\n";
+                            if(nTatli.Value > 0)
+                            {
+                                hesap += nTatli.Value * 50;
+                                mesaj += $"tatlı =   {nTatli.Value * 50}\r\n";
+                            }
+                            mesaj += $"***************************\r\n";
+                            mesaj += $"odenecek tutar    : {hesap}";
+
+                            txtBilgi.Text = mesaj;
+                        }
+                    }
+
+                }
+            }
+            
+        }
+    }
+}
